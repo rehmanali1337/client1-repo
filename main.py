8dbda67ce755bc152f73c7c4775b24f83f18bb80
Rehman Ali <rehmanali.9442289@gmail.com>
2022-11-30 10:01:38 +0500

    Added more fixes

--- main.py
+++ main.py
@@ -10,3 +10,8 @@
 """Removed all the features because client doesn't need them but I need this edit on main branch but not feature to be deleted"""
 
 """Ooppss.. client2 is good and made us develop some great feature."""
+
+
+"""Some hot change from client2 but only in main.py."""
+
+"""Client1 got some sense as well. Let's have all on master (for future)"""
